31439bb710738e9a3055ffba6ae2bd642def577e
= <=>
2026-01-12 17:23:06 -0300

    set wallpaper script

---
+++ scripts/.local/bin/set-wallpaper.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Diretório das imagens
+DIR="$HOME/Imagens/catppuccin-wallpapers/landscapes"
+
+# Lista os arquivos, remove o caminho completo para exibir apenas o nome no menu
+# e armazena a escolha do usuário
+CHOICE=$(ls "$DIR" | walker -d)
+
+# Se o usuário cancelar (ESC), o script encerra
+if [ -z "$CHOICE" ]; then
+  exit 1
+fi
+
+# Constrói o caminho completo do arquivo selecionado
+FULL_PATH="$DIR/$CHOICE"
+
+# Executa o comando do hyprctl
+# Primeiro 'preload' para garantir que a imagem está na memória, depois 'wallpaper'
+# hyprctl hyprpaper preload "$FULL_PATH"
+# hyprctl hyprpaper wallpaper ",$FULL_PATH"
+
+hyprctl hyprpaper wallpaper ",$FULL_PATH,cover"
